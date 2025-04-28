a1b5788ce8107a84c4203e4c6fd06eff6df8c2cd
이훤 <hubo0217@naver.com>
2025-04-28 10:52:57 +0900

    validbranch

--- router/posts.mjs
+++ router/posts.mjs
@@ -1,15 +1,23 @@
 import express from "express";
 import * as postController from "../controller/post.mjs";
+import { body } from "express-validator";
+import { validate } from "../middleware/validator.mjs";
 
 const router = express.Router();
+
+const validatePost = [
+  body("text").trim().isLength({ min: 5 }).withMessage("최소 5자이상 입력"),
+  validate,
+];
 
 // 모든 포스트 가져오기
 // 해당 아이디에 대한 포스트 가져오기
 // GET
+// http://127.0.0.1:8080/posts/
 // http://127.0.0.1:8080/posts?userid=apple
 router.get("/", postController.getPosts);
 
-// 글 번호에 대한 포스트 가져오기
+// 글번호에 대한 포스트 가져오기
 // GET
 // http://127.0.0.1:8080/posts/:id
 router.get("/:id", postController.getPost);
@@ -17,17 +25,18 @@ router.get("/:id", postController.getPost);
 // 포스트 쓰기
 // POST
 // http://127.0.0.1:8080/posts
-router.post("/", postController.createPost);
+// json 형태로 입력 후 저장
+router.post("/", validatePost, postController.createPost);
 
 // 포스트 수정하기
 // PUT
 // http://127.0.0.1:8080/posts/:id
 // json 형태로 입력 후 저장
-router.put(":id", postController.updatePost);
+router.put("/:id", validatePost, postController.updatePost);
 
 // 포스트 삭제하기
 // DELETE
 // http://127.0.0.1:8080/posts/:id
-router.delete(":id", postController.deletePost);
+router.delete("/:id", postController.deletePost);
 
 export default router;
