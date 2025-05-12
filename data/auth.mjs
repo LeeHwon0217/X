fc18035e3255ee9c2c6d5d2ffc632da5c2daff3b
이훤 <hubo0217@naver.com>
2025-05-12 11:58:14 +0900

    hh

---
+++ data/auth.mjs
@@ -0,0 +1,29 @@
+import Mongoose from "mongoose";
+import { useVirtualId } from "../db/database.mjs";
+
+const userSchema = new Mongoose.Schema(
+  {
+    userid: { type: String, require: true },
+    name: { type: String, require: true },
+    email: { type: String, require: true },
+    password: { type: String, require: true },
+    url: String,
+  },
+  { versionKey: false }
+);
+
+useVirtualId(userSchema);
+
+const User = Mongoose.model("User", userSchema);
+
+export async function createUser(user) {
+  return new User(user).save().then((data) => data.id);
+}
+
+export async function findByUserid(userid) {
+  return User.findOne({ userid });
+}
+
+export async function findByid(id) {
+  return User.findById(id);
+}
---
+++ data/post.mjs
@@ -0,0 +1,57 @@
+import Mongoose from "mongoose";
+import * as UserRepository from "./auth.mjs";
+import { useVirtualId } from "../db/database.mjs";
+
+const postSchema = new Mongoose.Schema(
+  {
+    userid: { type: String, require: true },
+    name: { type: String, require: true },
+    url: String,
+    text: { type: String, require: true },
+    userId: { type: String, require: true },
+  },
+  { timestamps: true }
+);
+
+useVirtualId(postSchema);
+
+const Post = Mongoose.model("Post", postSchema);
+
+// 모든 포스트를 리턴
+export async function getAll() {
+  return Post.find().sort({ createdAt: -1 });
+}
+
+// 사용자 아이디(userid)에 대한 포스트를 리턴
+// 조건을 만족하는 모든 요소를 배열로 리턴
+export async function getAllByUserid(userid) {
+  return Post.find({ userid }).sort({ createAt: -1 });
+}
+
+// 글 번호(id)에 대한 포스트를 리턴
+// 조건을 만족하는 첫 번째 요소 하나를 리턴
+export async function getById(id) {
+  return Post.findById(id);
+}
+
+// 포스트 작성
+export async function create(text, userId) {
+  return UserRepository.findByid(userId).then((user) =>
+    new Post({
+      userid: user.userid,
+      name: user.name,
+      userId,
+      text,
+    }).save()
+  );
+}
+
+// 포스트 변경
+export async function update(id, text) {
+  return Post.findByIdAndUpdate(id, { text }, { returnDocument: "after" });
+}
+
+// 포스트 삭제
+export async function remove(id) {
+  return Post.findByIdAndDelete(id);
+}
